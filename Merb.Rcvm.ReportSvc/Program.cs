69f6e726d28d46df1cc0062e4196aff7a30fce50
Martin Erik Rieland Bisgaard <mebisgaard@gmail.com>
2017-12-19 10:09:10 +0100

    Changed project type of the services

---
+++ Merb.Rcvm.ReportSvc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Merb.Rcvm.ReportSvc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
